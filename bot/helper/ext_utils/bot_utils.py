f35f318326f62be940c0e0a69c1c1152f69f5fdc
xbeet <84825812+xbeet@users.noreply.github.com>
2021-05-28 21:03:43 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -14,13 +14,13 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading...📥"
-    STATUS_WAITING = "Queued...📝"
+    STATUS_UPLOADING = "Uploading....📤"
+    STATUS_DOWNLOADING = "Downloading....📥"
+    STATUS_WAITING = "Queued....📝"
     STATUS_FAILED = "Failed 🚫. Cleaning Download..."
     STATUS_CANCELLED = "Cancelled ❌. Cleaning Download..."
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
+    STATUS_ARCHIVING = "Archiving....🔐"
+    STATUS_EXTRACTING = "Extracting....🗳"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
