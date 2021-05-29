fd50e36434034f1944ed6f414c91c29fde8b99c6
xbeet <84825812+xbeet@users.noreply.github.com>
2021-05-29 09:26:18 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -83,7 +83,7 @@ def ping(update, context):
     start_time = int(round(time.time() * 1000))
     reply = sendMessage("Starting Ping...🖲️", context.bot, update)
     end_time = int(round(time.time() * 1000))
-    editMessage(f'{end_time - start_time} ms', reply)
+    editMessage(f'{end_time - start_time} ms🖲️', reply)
 
 
 @run_async
