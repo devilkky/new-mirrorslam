315ca2248f1d48f995cc9688e44231db11c619b3
xbeet <84825812+xbeet@users.noreply.github.com>
2021-05-29 11:06:15 +0530

    Update __init__.py

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -7,7 +7,7 @@ from html_telegraph_poster import TelegraphPoster
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "ANonYmoUSFriEND-Nirrorbot"
+    auth_name = "ANonYmoUSFriEND-Mirrorbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
